3a8ef258ff2f8cb8f3a264dffb0603ff199a8db7
dionesb <diones.breda@gmail.com>
2019-07-15 15:38:39 -0300

    Created user migration

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: 'localhost',
   username: 'postgres',
   password: 'docker',
-  database: 'meetapp',
+  database: 'token_authentication',
   define: {
     timestamps: true,
     underscored: true,
