1051452dc188eab9d7a8804d3df46a623bd8d01b
dionesb <diones.breda@gmail.com>
2019-07-15 16:51:27 -0300

    Validation of input data added.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,24 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    /* Valida o objeto req.body conforme o schema, caso seja falso, retorna um
+    erro 400 */
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const userExist = await User.findOne({ where: { email: req.body.email } });
 
     if (userExist) {
