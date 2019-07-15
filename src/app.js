31732ee2de9ba134e0aca4b4f8598f1be8511cf5
dionesb <diones.breda@gmail.com>
2019-07-15 14:39:56 -0300

    Server testing and config. nodemon and sucrase.

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+
+import routes from './routes';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+    this.exceptionHandler();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+
+  exceptionHandler() {}
+}
+
+export default new App().server;
